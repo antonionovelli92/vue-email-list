f758a9f256b5b461f7aa0008e3567914de79dedd
antonionovelli92 <anto.novelli@hotmail.it>
2023-01-18 16:51:45 +0100

    end

--- js/vuejs.js
+++ js/vuejs.js
@@ -3,12 +3,24 @@ console.log('Vue ok', Vue);
 const app = Vue.createApp({
     data() {
         return {
-            mail: []
-
+            mails: [],
+            mailUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
         }
     },
-    mounted() {
+    methods: {
+        getRandomMail() {
+            axios.get(this.mailUrl)
+                .then((response) => {
+                    this.mails.push(response.data.response);
+                    console.log(this.mails)
+                })
+        }
 
+    },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            this.getRandomMail();
+        }
     }
 });
 
